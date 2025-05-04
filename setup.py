a1e04186769ebbbe10acb24fdd69a8d11e2d4b0d
gouridumale <gouri.dumale@gmail.com>
2025-05-03 22:55:22 -0400

    folderstructure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative Ai Project',
+    version = '0.0.0',
+    author = 'Gouri Dumale',
+    author_email = 'gouri.dumale@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+
+)
